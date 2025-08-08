856ad3d76e7e4008512c6e5ae4208397ac760798
JacksonZhang <41615769+JacksonZhang95@users.noreply.github.com>
2025-08-08 09:59:51 +0800

    Update start.sh
    Signed-off-by: JacksonZhang <41615769+JacksonZhang95@users.noreply.github.com>

--- genie-backend/start.sh
+++ genie-backend/start.sh
@@ -7,4 +7,8 @@ MAIN_MODULE="com.jd.genie.GenieApplication"
 LOGFILE="./genie-backend_startup.log"
 
 echo "starting $APP_NAME :)"
-java -classpath "$CLASSPATH" -Dbasedir="$BASEDIR" -Dfile.encoding="UTF-8" ${MAIN_MODULE} > $LOGFILE 2>&1 &
+echo "starting $ENV_BASEURL :)"
+echo "starting $ENV_APIKEY :)"
+echo "starting $ENV_MODEL :)"
+echo "starting $ENV_MAXTOKEN :)"
+java -Dserver.port=${ENV_PORT} -Dllm.default.base_url=${ENV_BASEURL} -Dllm.default.apikey=${ENV_APIKEY} -Dllm.default.model=${ENV_MODEL} -Dllm.default.max_tokens=${ENV_MAXTOKEN} -classpath "$CLASSPATH" -Dbasedir="$BASEDIR" -Dfile.encoding="UTF-8" ${MAIN_MODULE} > $LOGFILE 2>&1 &
